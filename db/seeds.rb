92b38f9877ba142fb7c7995fcf1cdb7648a217df
Shin Nagai <>
2017-11-08 20:12:11 +0900

    add book

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do
-   Note.create(title: "Hello!")
-end
